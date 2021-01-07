3f9863ca6c2fa5055d06c0c9300c8694141dabba
hajar <hajar.etta.7@gmail.com>
2021-01-07 22:40:22 +0100

    'trying'

--- dashboard.py
+++ dashboard.py
@@ -33,7 +33,7 @@ def create_plot_chart():
                       marker_line_width=1.5, opacity=0.6)
     fig.update_layout(barmode='stack', yaxis={'categoryorder': 'total descending'})
     return fig
-fig1 = create_plot_chart()
+fig11 = create_plot_chart()
 
 
 def create_plot_pie():
@@ -44,7 +44,7 @@ def create_plot_pie():
     fig_2 = go.Figure(data=[go.Pie(labels=df['Country,\nOther'][:20].dropna(), values=df['New\nDeaths'][0:20].dropna(), hole=.3)])
     fig_2.update_layout(title='Sunburst Chart')
     return fig_2
-fig2 = create_plot_pie()
+fig10 = create_plot_pie()
 
 
 def create_plot_chart_2():
@@ -63,7 +63,7 @@ def create_plot_chart_2():
 
     fig.update_layout(barmode='stack', yaxis={'categoryorder': 'total descending'})
     return fig
-fig3 = create_plot_chart_2()
+fig9 = create_plot_chart_2()
 
 
 def create_plot_4():
@@ -97,34 +97,6 @@ def create_plot_5():
 fig5 = create_plot_5()
 
 
-def create_plot_6():
-    df['text'] = df.apply(lambda r: "Deaths: " + str(r['Total\nDeaths']) + " Suspected: " + " " + str(r['New\nCases']), axis=1)
-    country_code = pd.read_csv("countrycode.csv")
-    # Convert the dataframe to dictionary
-    country_code.set_index('COUNTRY', inplace=True)
-    dict_country_code = country_code.to_dict()
-    REPLACE_LIST = dict_country_code['CODE']
-    # Replace Country with Codes
-    df.replace(REPLACE_LIST, inplace=True)
-
-    data = dict(
-        type='choropleth',
-        colorscale='ylorrd',
-        locations=df['Country,\nOther'].values,
-        z=df['Total\nCases'],
-        text=df['text'],
-        colorbar={'title': 'Corona Total Cases'},
-    )
-    layout = dict(
-        title='Global Corona stats',
-        geo=dict(
-            showframe=False,
-            projection={'type': 'natural earth'}
-        )
-    )
-    fig = go.Figure(data=[data], layout=layout)
-    return fig
-fig6 = create_plot_6()
 
 
 def create_plot_7():
@@ -153,21 +125,21 @@ def create_plot_9():
     fig.update_layout(title='Bubble Plot for Total Cases v Total Deaths of 50 Most Affected Countries',
                       xaxis_title='Cases', yaxis_title='Deaths')
     return fig
-fig9 = create_plot_9()
+fig3 = create_plot_9()
 
 def create_plot_10():
     fig = px.scatter_3d(worldometer.head(20), x='TotalCases', y='TotalDeaths', z='TotalRecovered',
                         color='Country/Region')
     fig.update_layout(title='3D Plot of Total Cases, Total Deaths and Total Recovered of Top 20 Affected Countries')
     return fig
-fig10 = create_plot_10()
+fig2 = create_plot_10()
 
 def create_plot_11():
     fig = px.bar(worldometer.head(10), y='Tests/1M pop', x='Country/Region', color='WHO Region', height=400)
     fig.update_layout(title='Comparison of Tests/Million of 10 Most Affected Countries', xaxis_title='Country',
                       yaxis_title='Tests/Million', template="plotly_dark")
     return fig
-fig11 = create_plot_11()
+fig1 = create_plot_11()
 
 
 
@@ -179,11 +151,10 @@ def index():
     figures.append(fig3)
     figures.append(fig4)
     figures.append(fig5)
-    figures.append(fig6)
     figures.append(fig7)
-    figures.append(fig8)
-    figures.append(fig9)
     figures.append(fig10)
+    figures.append(fig9)
+    figures.append(fig8)
     figures.append(fig11)
     #figures.append(fig12)
     ids = ['figure-{}'.format(i) for i, _ in enumerate(figures)]
